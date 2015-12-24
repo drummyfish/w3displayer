a1822213d499b3d9d9aa8b4a34966d56a8327e8c
Miloslav Číž <tastyfish@seznam.cz>
2015-12-24 20:27:28 +0100

    added item actions

--- analyse.py
+++ analyse.py
@@ -7,6 +7,8 @@ TIME_STEP = 500             # time of periodic update, in miliseconds
 ACTIONS_SHOWN = 5           # how many last player action to show
 RECENTLY_USED_COUNTDOWN = 3 # for how many frames a recently used ability will be shown
 
+TIER_UPGRADE_TIME = 140000;
+
 HERO_REVIVE_TIMES = {  # revive times of heroes by level  
     0 :  55000,
     1 :  35750,
@@ -270,7 +272,247 @@ ACTION_COSTS = {       # gold, lumber, food
   w3g.ITEMS[b'Npld'] : (0,0,0),     # pit lord
   w3g.ITEMS[b'Ntin'] : (0,0,0),     # goblin tinker
   w3g.ITEMS[b'Nfir'] : (0,0,0),     # firelord
-  w3g.ITEMS[b'Nalc'] : (0,0,0)      # goblin alchemist
+  w3g.ITEMS[b'Nalc'] : (0,0,0),     # goblin alchemist
+  
+  w3g.ITEMS[b'amrc'] : (0,0,0),     # amulet of recall
+  w3g.ITEMS[b'ankh'] : (0,0,0),     # ankh of reincarnation
+  w3g.ITEMS[b'belv'] : (0,0,0),     # boots of quel\'thalas +6
+  w3g.ITEMS[b'bgst'] : (0,0,0),     # belt of giant strength +6
+  w3g.ITEMS[b'bspd'] : (0,0,0),     # boots of speed
+  w3g.ITEMS[b'ccmd'] : (0,0,0),     # scepter of mastery
+  w3g.ITEMS[b'ciri'] : (0,0,0),     # robe of the magi +6
+  w3g.ITEMS[b'ckng'] : (0,0,0),     # crown of kings +5
+  w3g.ITEMS[b'clsd'] : (0,0,0),     # cloak of shadows
+  w3g.ITEMS[b'crys'] : (0,0,0),     # crystal ball
+  w3g.ITEMS[b'desc'] : (0,0,0),     # kelen's dagger of escape
+  w3g.ITEMS[b'gemt'] : (0,0,0),     # gem of true seeing
+  w3g.ITEMS[b'gobm'] : (0,0,0),     # goblin land mines
+  w3g.ITEMS[b'gsou'] : (0,0,0),     # soul gem
+  w3g.ITEMS[b'guvi'] : (0,0,0),     # glyph of ultravision
+  w3g.ITEMS[b'gfor'] : (0,0,0),     # glyph of fortification
+  w3g.ITEMS[b'soul'] : (0,0,0),     # soul
+  w3g.ITEMS[b'mdpb'] : (0,0,0),     # medusa pebble
+  w3g.ITEMS[b'rag1'] : (0,0,0),     # slippers of agility +3
+  w3g.ITEMS[b'rat3'] : (0,0,0),     # claws of attack +3
+  w3g.ITEMS[b'rin1'] : (0,0,0),     # mantle of intelligence +3
+  w3g.ITEMS[b'rde1'] : (0,0,0),     # ring of protection +2
+  w3g.ITEMS[b'rde2'] : (0,0,0),     # ring of protection +3
+  w3g.ITEMS[b'rde3'] : (0,0,0),     # ring of protection +4
+  w3g.ITEMS[b'rhth'] : (0,0,0),     # khadgar's gem of health
+  w3g.ITEMS[b'rst1'] : (0,0,0),     # gauntlets of ogre strength +3
+  w3g.ITEMS[b'ofir'] : (0,0,0),     # orb of fire
+  w3g.ITEMS[b'ofro'] : (0,0,0),     # orb of frost
+  w3g.ITEMS[b'olig'] : (0,0,0),     # orb of lightning
+  w3g.ITEMS[b'oli2'] : (0,0,0),     # orb of lightning
+  w3g.ITEMS[b'oven'] : (0,0,0),     # orb of venom
+  w3g.ITEMS[b'odef'] : (0,0,0),     # orb of darkness
+  w3g.ITEMS[b'ocor'] : (0,0,0),     # orb of corruption
+  w3g.ITEMS[b'pdiv'] : (0,0,0),     # potion of divinity
+  w3g.ITEMS[b'phea'] : (0,0,0),     # potion of healing
+  w3g.ITEMS[b'pghe'] : (0,0,0),     # potion of greater healing
+  w3g.ITEMS[b'pinv'] : (0,0,0),     # potion of invisibility
+  w3g.ITEMS[b'pgin'] : (0,0,0),     # potion of greater invisibility
+  w3g.ITEMS[b'pman'] : (0,0,0),     # potion of mana
+  w3g.ITEMS[b'pgma'] : (0,0,0),     # potion of greater mana
+  w3g.ITEMS[b'pnvu'] : (0,0,0),     # potion of invulnerability
+  w3g.ITEMS[b'pnvl'] : (0,0,0),     # potion of lesser invulnerability
+  w3g.ITEMS[b'pres'] : (0,0,0),     # potion of restoration
+  w3g.ITEMS[b'pspd'] : (0,0,0),     # potion of speed
+  w3g.ITEMS[b'rlif'] : (0,0,0),     # ring of regeneration
+  w3g.ITEMS[b'rwiz'] : (0,0,0),     # sobi mask
+  w3g.ITEMS[b'sfog'] : (0,0,0),     # horn of the clouds
+  w3g.ITEMS[b'shea'] : (0,0,0),     # scroll of healing
+  w3g.ITEMS[b'sman'] : (0,0,0),     # scroll of mana
+  w3g.ITEMS[b'spro'] : (0,0,0),     # scroll of protection
+  w3g.ITEMS[b'sres'] : (0,0,0),     # scroll of restoration
+  w3g.ITEMS[b'ssil'] : (0,0,0),     # staff of silence
+  w3g.ITEMS[b'stwp'] : (0,0,0),     # scroll of town portal
+  w3g.ITEMS[b'tels'] : (0,0,0),     # goblin night scope
+  w3g.ITEMS[b'tdex'] : (0,0,0),     # tome of agility
+  w3g.ITEMS[b'texp'] : (0,0,0),     # tome of experience
+  w3g.ITEMS[b'tint'] : (0,0,0),     # tome of intelligence
+  w3g.ITEMS[b'tkno'] : (0,0,0),     # tome of power
+  w3g.ITEMS[b'tstr'] : (0,0,0),     # tome of strength
+  w3g.ITEMS[b'ward'] : (0,0,0),     # warsong battle drums
+  w3g.ITEMS[b'will'] : (0,0,0),     # wand of illusion
+  w3g.ITEMS[b'wneg'] : (0,0,0),     # wand of negation
+  w3g.ITEMS[b'rdis'] : (0,0,0),     # rune of dispel magic
+  w3g.ITEMS[b'rwat'] : (0,0,0),     # rune of the watcher
+  w3g.ITEMS[b'fgrd'] : (0,0,0),     # red drake egg
+  w3g.ITEMS[b'fgrg'] : (0,0,0),     # stone token
+  w3g.ITEMS[b'fgdg'] : (0,0,0),     # demonic figurine
+  w3g.ITEMS[b'fgfh'] : (0,0,0),     # spiked collar
+  w3g.ITEMS[b'fgsk'] : (0,0,0),     # book of the dead
+  w3g.ITEMS[b'engs'] : (0,0,0),     # enchanted gemstone
+  w3g.ITEMS[b'k3m1'] : (0,0,0),     # mooncrystal
+  w3g.ITEMS[b'modt'] : (0,0,0),     # mask of death
+  w3g.ITEMS[b'sand'] : (0,0,0),     # scroll of animate dead
+  w3g.ITEMS[b'srrc'] : (0,0,0),     # scroll of resurrection
+  w3g.ITEMS[b'sror'] : (0,0,0),     # scroll of the beast
+  w3g.ITEMS[b'infs'] : (0,0,0),     # inferno stone
+  w3g.ITEMS[b'shar'] : (0,0,0),     # ice shard
+  w3g.ITEMS[b'wild'] : (0,0,0),     # amulet of the wild
+  w3g.ITEMS[b'wswd'] : (0,0,0),     # sentry wards
+  w3g.ITEMS[b'whwd'] : (0,0,0),     # healing wards
+  w3g.ITEMS[b'wlsd'] : (0,0,0),     # wand of lightning shield
+  w3g.ITEMS[b'wcyc'] : (0,0,0),     # wand of the wind
+  w3g.ITEMS[b'rnec'] : (0,0,0),     # rod of necromancy
+  w3g.ITEMS[b'pams'] : (0,0,0),     # anti-magic potion
+  w3g.ITEMS[b'clfm'] : (0,0,0),     # cloak of flames
+  w3g.ITEMS[b'evtl'] : (0,0,0),     # talisman of evasion
+  w3g.ITEMS[b'nspi'] : (0,0,0),     # necklace of spell immunity
+  w3g.ITEMS[b'lhst'] : (0,0,0),     # the lion horn of stormwind
+  w3g.ITEMS[b'kpin'] : (0,0,0),     # khadgar's pipe of insight
+  w3g.ITEMS[b'sbch'] : (0,0,0),     # scourge bone chimes
+  w3g.ITEMS[b'afac'] : (0,0,0),     # alleria's flute of accuracy
+  w3g.ITEMS[b'ajen'] : (0,0,0),     # ancient janggo of endurance
+  w3g.ITEMS[b'lgdh'] : (0,0,0),     # legion doom-horn
+  w3g.ITEMS[b'hcun'] : (0,0,0),     # hood of cunning
+  w3g.ITEMS[b'mcou'] : (0,0,0),     # medallion of courage
+  w3g.ITEMS[b'hval'] : (0,0,0),     # helm of valor
+  w3g.ITEMS[b'cnob'] : (0,0,0),     # circlet of nobility
+  w3g.ITEMS[b'prvt'] : (0,0,0),     # periapt of vitality
+  w3g.ITEMS[b'tgxp'] : (0,0,0),     # tome of greater experience
+  w3g.ITEMS[b'mnst'] : (0,0,0),     # mana stone
+  w3g.ITEMS[b'hlst'] : (0,0,0),     # health stone
+  w3g.ITEMS[b'tpow'] : (0,0,0),     # tome of knowledge
+  w3g.ITEMS[b'tst2'] : (0,0,0),     # tome of strength +2
+  w3g.ITEMS[b'tin2'] : (0,0,0),     # tome of intelligence +2
+  w3g.ITEMS[b'tdx2'] : (0,0,0),     # tome of agility +2
+  w3g.ITEMS[b'rde0'] : (0,0,0),     # ring of protection +1
+  w3g.ITEMS[b'rde4'] : (0,0,0),     # ring of protection +5
+  w3g.ITEMS[b'rat6'] : (0,0,0),     # claws of attack +6
+  w3g.ITEMS[b'rat9'] : (0,0,0),     # claws of attack +9
+  w3g.ITEMS[b'ratc'] : (0,0,0),     # claws of attack +12
+  w3g.ITEMS[b'ratf'] : (0,0,0),     # claws of attack +15
+  w3g.ITEMS[b'manh'] : (0,0,0),     # manual of health
+  w3g.ITEMS[b'pmna'] : (0,0,0),     # pendant of mana
+  w3g.ITEMS[b'penr'] : (0,0,0),     # pendant of energy
+  w3g.ITEMS[b'gcel'] : (0,0,0),     # gloves of haste
+  w3g.ITEMS[b'totw'] : (0,0,0),     # talisman of the wild
+  w3g.ITEMS[b'phlt'] : (0,0,0),     # phat lewt
+  w3g.ITEMS[b'gopr'] : (0,0,0),     # glyph of purification
+  w3g.ITEMS[b'ches'] : (0,0,0),     # cheese
+  w3g.ITEMS[b'mlst'] : (0,0,0),     # maul of strength
+  w3g.ITEMS[b'rnsp'] : (0,0,0),     # ring of superiority
+  w3g.ITEMS[b'brag'] : (0,0,0),     # bracer of agility
+  w3g.ITEMS[b'sksh'] : (0,0,0),     # skull shield
+  w3g.ITEMS[b'vddl'] : (0,0,0),     # voodoo doll
+  w3g.ITEMS[b'sprn'] : (0,0,0),     # spider ring
+  w3g.ITEMS[b'tmmt'] : (0,0,0),     # totem of might
+  w3g.ITEMS[b'anfg'] : (0,0,0),     # ancient figurine
+  w3g.ITEMS[b'lnrn'] : (0,0,0),     # lion\'s ring
+  w3g.ITEMS[b'iwbr'] : (0,0,0),     # ironwood branch
+  w3g.ITEMS[b'jdrn'] : (0,0,0),     # jade ring
+  w3g.ITEMS[b'drph'] : (0,0,0),     # druid pouch
+  w3g.ITEMS[b'hslv'] : (0,0,0),     # healing salve
+  w3g.ITEMS[b'pclr'] : (0,0,0),     # clarity potion
+  w3g.ITEMS[b'plcl'] : (0,0,0),     # lesser clarity potion
+  w3g.ITEMS[b'rej1'] : (0,0,0),     # minor replenishment potion
+  w3g.ITEMS[b'rej2'] : (0,0,0),     # lesser replenishment potion
+  w3g.ITEMS[b'rej3'] : (0,0,0),     # replenishment potion
+  w3g.ITEMS[b'rej4'] : (0,0,0),     # greater replenishment potion
+  w3g.ITEMS[b'rej5'] : (0,0,0),     # lesser scroll of replenishment
+  w3g.ITEMS[b'rej6'] : (0,0,0),     # greater scroll of replenishment
+  w3g.ITEMS[b'sreg'] : (0,0,0),     # scroll of regeneration
+  w3g.ITEMS[b'gold'] : (0,0,0),     # gold coins
+  w3g.ITEMS[b'lmbr'] : (0,0,0),     # bundle of lumber
+  w3g.ITEMS[b'fgun'] : (0,0,0),     # flare gun
+  w3g.ITEMS[b'pomn'] : (0,0,0),     # potion of omniscience
+  w3g.ITEMS[b'gomn'] : (0,0,0),     # glyph of omniscience
+  w3g.ITEMS[b'wneu'] : (0,0,0),     # wand of neutralization
+  w3g.ITEMS[b'silk'] : (0,0,0),     # spider silk broach
+  w3g.ITEMS[b'lure'] : (0,0,0),     # monster lure
+  w3g.ITEMS[b'skul'] : (0,0,0),     # sacrificial skull
+  w3g.ITEMS[b'moon'] : (0,0,0),     # moonstone
+  w3g.ITEMS[b'brac'] : (0,0,0),     # runed bracers
+  w3g.ITEMS[b'vamp'] : (0,0,0),     # vampiric potion
+  w3g.ITEMS[b'woms'] : (0,0,0),     # wand of mana stealing
+  w3g.ITEMS[b'tcas'] : (0,0,0),     # tiny castle
+  w3g.ITEMS[b'tgrh'] : (0,0,0),     # tiny great hall
+  w3g.ITEMS[b'tsct'] : (0,0,0),     # ivory tower
+  w3g.ITEMS[b'wshs'] : (0,0,0),     # wand of shadowsight
+  w3g.ITEMS[b'tret'] : (0,0,0),     # tome of retraining
+  w3g.ITEMS[b'sneg'] : (0,0,0),     # staff of negation
+  w3g.ITEMS[b'stel'] : (0,0,0),     # staff of teleportation
+  w3g.ITEMS[b'spre'] : (0,0,0),     # staff of preservation
+  w3g.ITEMS[b'mcri'] : (0,0,0),     # mechanical critter
+  w3g.ITEMS[b'spsh'] : (0,0,0),     # amulet of spell shield
+  w3g.ITEMS[b'sbok'] : (0,0,0),     # spell book
+  w3g.ITEMS[b'ssan'] : (0,0,0),     # staff of sanctuary
+  w3g.ITEMS[b'shas'] : (0,0,0),     # scroll of speed
+  w3g.ITEMS[b'dust'] : (0,0,0),     # dust of appearance
+  w3g.ITEMS[b'oslo'] : (0,0,0),     # orb of slow
+  w3g.ITEMS[b'dsum'] : (0,0,0),     # diamond of summoning
+  w3g.ITEMS[b'sor1'] : (0,0,0),     # shadow orb +1
+  w3g.ITEMS[b'sor2'] : (0,0,0),     # shadow orb +2
+  w3g.ITEMS[b'sor3'] : (0,0,0),     # shadow orb +3
+  w3g.ITEMS[b'sor4'] : (0,0,0),     # shadow orb +4
+  w3g.ITEMS[b'sor5'] : (0,0,0),     # shadow orb +5
+  w3g.ITEMS[b'sor6'] : (0,0,0),     # shadow orb +6
+  w3g.ITEMS[b'sor7'] : (0,0,0),     # shadow orb +7
+  w3g.ITEMS[b'sor8'] : (0,0,0),     # shadow orb +8
+  w3g.ITEMS[b'sor9'] : (0,0,0),     # shadow orb +9
+  w3g.ITEMS[b'sora'] : (0,0,0),     # shadow orb +10
+  w3g.ITEMS[b'sorf'] : (0,0,0),     # shadow orb fragment
+  w3g.ITEMS[b'fwss'] : (0,0,0),     # frost wyrm skull shield
+  w3g.ITEMS[b'ram1'] : (0,0,0),     # ring of the archmagi
+  w3g.ITEMS[b'ram2'] : (0,0,0),     # ring of the archmagi
+  w3g.ITEMS[b'ram3'] : (0,0,0),     # ring of the archmagi
+  w3g.ITEMS[b'ram4'] : (0,0,0),     # ring of the archmagi
+  w3g.ITEMS[b'shtm'] : (0,0,0),     # shamanic totem
+  w3g.ITEMS[b'shwd'] : (0,0,0),     # shimmerweed
+  w3g.ITEMS[b'btst'] : (0,0,0),     # battle standard
+  w3g.ITEMS[b'skrt'] : (0,0,0),     # skeletal artifact
+  w3g.ITEMS[b'thle'] : (0,0,0),     # thunder lizard egg
+  w3g.ITEMS[b'sclp'] : (0,0,0),     # secret level powerup
+  w3g.ITEMS[b'gldo'] : (0,0,0),     # orb of kil'jaeden
+  w3g.ITEMS[b'tbsm'] : (0,0,0),     # tiny blacksmith
+  w3g.ITEMS[b'tfar'] : (0,0,0),     # tiny farm
+  w3g.ITEMS[b'tlum'] : (0,0,0),     # tiny lumber mill
+  w3g.ITEMS[b'tbar'] : (0,0,0),     # tiny barracks
+  w3g.ITEMS[b'tbak'] : (0,0,0),     # tiny altar of kings
+  w3g.ITEMS[b'mgtk'] : (0,0,0),     # magic key chain
+  w3g.ITEMS[b'stre'] : (0,0,0),     # staff of reanimation
+  w3g.ITEMS[b'horl'] : (0,0,0),     # sacred relic
+  w3g.ITEMS[b'hbth'] : (0,0,0),     # helm of battlethirst
+  w3g.ITEMS[b'blba'] : (0,0,0),     # bladebane armor
+  w3g.ITEMS[b'rugt'] : (0,0,0),     # runed gauntlets
+  w3g.ITEMS[b'frhg'] : (0,0,0),     # firehand gauntlets
+  w3g.ITEMS[b'gvsm'] : (0,0,0),     # gloves of spell mastery
+  w3g.ITEMS[b'crdt'] : (0,0,0),     # crown of the deathlord
+  w3g.ITEMS[b'arsc'] : (0,0,0),     # arcane scroll
+  w3g.ITEMS[b'scul'] : (0,0,0),     # scroll of the unholy legion
+  w3g.ITEMS[b'tmsc'] : (0,0,0),     # tome of sacrifices
+  w3g.ITEMS[b'dtsb'] : (0,0,0),     # drek'thar's spellbook
+  w3g.ITEMS[b'grsl'] : (0,0,0),     # grimoire of souls
+  w3g.ITEMS[b'arsh'] : (0,0,0),     # arcanite shield
+  w3g.ITEMS[b'shdt'] : (0,0,0),     # shield of the deathlord
+  w3g.ITEMS[b'shhn'] : (0,0,0),     # shield of honor
+  w3g.ITEMS[b'shen'] : (0,0,0),     # enchanted shield
+  w3g.ITEMS[b'thdm'] : (0,0,0),     # thunderlizard diamond
+  w3g.ITEMS[b'stpg'] : (0,0,0),     # clockwork penguin
+  w3g.ITEMS[b'shrs'] : (0,0,0),     # shimmerglaze roast
+  w3g.ITEMS[b'bfhr'] : (0,0,0),     # bloodfeather's heart
+  w3g.ITEMS[b'cosl'] : (0,0,0),     # celestial orb of souls
+  w3g.ITEMS[b'shcw'] : (0,0,0),     # shaman claws
+  w3g.ITEMS[b'srbd'] : (0,0,0),     # searing blade
+  w3g.ITEMS[b'frgd'] : (0,0,0),     # frostguard
+  w3g.ITEMS[b'envl'] : (0,0,0),     # enchanted vial
+  w3g.ITEMS[b'rump'] : (0,0,0),     # rusty mining pick
+  w3g.ITEMS[b'mort'] : (0,0,0),     # mogrin's report
+  w3g.ITEMS[b'srtl'] : (0,0,0),     # serathil
+  w3g.ITEMS[b'stwa'] : (0,0,0),     # sturdy war axe
+  w3g.ITEMS[b'klmm'] : (0,0,0),     # killmaim
+  w3g.ITEMS[b'rots'] : (0,0,0),     # scepter of the sea
+  w3g.ITEMS[b'axas'] : (0,0,0),     # ancestral staff
+  w3g.ITEMS[b'mnsf'] : (0,0,0),     # mindstaff
+  w3g.ITEMS[b'schl'] : (0,0,0),     # scepter of healing
+  w3g.ITEMS[b'asbl'] : (0,0,0),     # assassin's blade
+  w3g.ITEMS[b'kgal'] : (0,0,0),     # keg of ale
+  w3g.ITEMS[b'dphe'] : (0,0,0),     # thunder phoenix egg
+  w3g.ITEMS[b'dkfw'] : (0,0,0),     # keg of thunderwater
+  w3g.ITEMS[b'dthb'] : (0,0,0),     # thunderbloom bulb
   }
 
 class W3JSONEncoder(json.JSONEncoder):
@@ -307,10 +549,12 @@ class Hero:
 
 class PlayerState:
   def __init__(self):
-    self.heroes = []      # will hold Hero objects
+    self.heroes = []             # will hold Hero objects
     self.gold_spent = 0
     self.lumber_spent = 0
     self.last_actions = ["" for i in range(ACTIONS_SHOWN)]
+    self.tier = 1
+    self.tier_time_left = -1     # how long till next tier
 
 class Player:
   def __init__(self):
@@ -453,6 +697,8 @@ for event in replay_file.events:
 
     if ability_item in ACTION_COSTS:
       player.add_action(ability_item)
+    elif ability_item.find("Use item") >= 0:
+      player.add_action("item used")
 
     if trained_ability != None:
       hero = player.get_hero_by_name(trained_ability[0])
@@ -466,6 +712,3 @@ for event in replay_file.events:
           new_hero.revive_time_left = HERO_REVIVE_TIMES[0]
         
         player.state.heroes.append(new_hero)
-        
-  #  print(e.time)
-  #  print(w3g.ITEMS.get(e.ability, e.ability))
